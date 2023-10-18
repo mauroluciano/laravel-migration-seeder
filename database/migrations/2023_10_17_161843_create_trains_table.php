89b4363270d482409d2b792eca430f128e8d3cc7
mauroluciano <mauroluciano911@gmail.com>
2023-10-18 10:16:36 +0200

    table

--- database/migrations/2023_10_17_161843_create_trains_table.php
+++ database/migrations/2023_10_17_161843_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('agency', 100);
+            $table->string('departing_from', 50);
+            $table->string('going_to', 50);
+            $table->string('leaving_time', 15);
+            $table->string('arrives_time', 15);
+            $table->string('id_train', 15);
+            $table->string('coach', 15);
+            $table->string('time', 15);
+            $table->string('deleted', 15);       
             $table->timestamps();
         });
     }
